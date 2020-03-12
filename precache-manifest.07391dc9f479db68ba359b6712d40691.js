3124b42aaa17cefa87790bb4c699e35648228147
Jonathan Ng <ngkjon@gmail.com>
2020-03-11 23:57:58 -0400

    Updates

--- precache-manifest.07391dc9f479db68ba359b6712d40691.js
+++ precache-manifest.07391dc9f479db68ba359b6712d40691.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ad02a9d66de2b187f91da5305d5609f4",
+    "revision": "3b675e4bbe03ffa0446097c2b263a0b9",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "156a5a4ed8b2bbc0ef3a",
-    "url": "/portfolio/static/css/main.4d7abd06.chunk.css"
+    "revision": "2de209846823b4ef7298",
+    "url": "/portfolio/static/css/main.7eebd9ab.chunk.css"
   },
   {
     "revision": "bc8983ae11aa1c9b55cf",
     "url": "/portfolio/static/js/2.8ad86e4e.chunk.js"
   },
   {
-    "revision": "156a5a4ed8b2bbc0ef3a",
-    "url": "/portfolio/static/js/main.51381e9a.chunk.js"
+    "revision": "2de209846823b4ef7298",
+    "url": "/portfolio/static/js/main.83b0a4c8.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b53ae91b1edb4bc4d52b10bd59630e90.js"
+  "/portfolio/precache-manifest.07391dc9f479db68ba359b6712d40691.js"
 );
 
 self.addEventListener('message', (event) => {
